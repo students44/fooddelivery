2cba5d8b5e4a2c289b978eba3162fb8e2b47e7a2
Muneeb khan <itstudents005@gmail.com>
2025-08-04 02:35:32 -0700

    testing

--- fooddelivery/src/Components/Navbar/Navbar.jsx
+++ fooddelivery/src/Components/Navbar/Navbar.jsx
@@ -6,15 +6,22 @@ const Navbar = () => {
   return (
     <div className='navbar'>
      <img src={assets.logo} alt=""  width={120} height={120}/>
-     <ul className="menu-bar">
+     <ul className="menu-list">
       <li>Home</li>
       <li>Menu</li>
       <li>Mobile-App</li>
       <li>Contact Us</li>
      </ul>
 
-     <div className="navbar-ri">
+     <div className="navbar-right">
       <img src={assets.search_icon} alt="Search icon" />
+
+      <div className='navbar-search-icon'>
+        <img src={assets.basket_icon} alt=" basket icon" />
+        <div className='dot'></div>
+      </div>
+
+       <button className='sign-in'>Sign in</button>
      </div>
     </div>
   )
