5fea07b1a5717b01a18267d2aa25e87e143fedb8
Muneeb khan <itstudents005@gmail.com>
2025-08-05 06:42:32 -0700

    add and removed funtionality complete

--- fooddelivery/src/Components/Fooditem/FoodItem.jsx
+++ fooddelivery/src/Components/Fooditem/FoodItem.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
+import React ,{useState} from 'react'
 import './FoodItem.css'
 import { assets } from '../../assets/frontend_assets/assets'
 
 const FoodItem = (props) => {
+   
+
+    const [itemCount, setItemCount] = useState(0);
+    console.log(itemCount);
+
     let {_id:id,name,price,description,image} = props.item; // Destructure the item object
   return (
     <div className='food-item'>
       <div className="food-item-image-container">
         <img className='food-item-image' src={image} alt="" />
+
+
+    {
+        !itemCount ? <img className='add' onClick={()=>setItemCount(prev=>prev +1)} src={assets.add_icon_white} alt=''  /> 
+        : <div className='food-item-count'>
+            <img onClick={()=>setItemCount(prev=>prev-1)} src={assets.remove_icon_red} alt="" />
+            <p>{itemCount}</p>
+            <img onClick={()=>setItemCount(prev=>prev+1)} src={assets.add_icon_green} alt="" />
+          </div>
+    }
+
+
+
       </div>
 
         <div className="food-item-info"> 
