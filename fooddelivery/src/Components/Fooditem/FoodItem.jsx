243d93ed69f6129cfd63f6dc9af169b2b6e5973b
Muneeb khan <itstudents005@gmail.com>
2025-08-06 02:44:29 -0700

    add to cart funtionality complete

--- fooddelivery/src/Components/Fooditem/FoodItem.jsx
+++ fooddelivery/src/Components/Fooditem/FoodItem.jsx
@@ -1,48 +1,48 @@
-import React ,{useState} from 'react'
-import './FoodItem.css'
-import { assets } from '../../assets/frontend_assets/assets'
+import React, { useState, useContext } from 'react';
+import './FoodItem.css';
+import { assets } from '../../assets/frontend_assets/assets';
+import { StoreContext } from '../../context/StoreContext'; // Make sure the path is correct
 
 const FoodItem = (props) => {
-   
+    const { cartItems, addToCart, removeFromCart } = useContext(StoreContext);
 
-    const [itemCount, setItemCount] = useState(0);
-    console.log(itemCount);
+    const { _id: id, name, price, description, image } = props.item;
 
-    let {_id:id,name,price,description,image} = props.item; // Destructure the item object
-  return (
-    <div className='food-item'>
-      <div className="food-item-image-container">
-        <img className='food-item-image' src={image} alt="" />
+    return (
+        <div className='food-item'>
+            <div className="food-item-image-container">
+                <img className='food-item-image' src={image} alt="" />
 
-
-    {
-        !itemCount ? <img className='add' onClick={()=>setItemCount(prev=>prev +1)} src={assets.add_icon_white} alt=''  /> 
-        : <div className='food-item-count'>
-            <img onClick={()=>setItemCount(prev=>prev-1)} src={assets.remove_icon_red} alt="" />
-            <p>{itemCount}</p>
-            <img onClick={()=>setItemCount(prev=>prev+1)} src={assets.add_icon_green} alt="" />
-          </div>
-    }
-
-
-
-      </div>
-
-        <div className="food-item-info"> 
-            <div className="food-item-name-rating">
-                <p>{name}</p>
-                <img src={assets.rating_starts} alt="" />
+                {
+                    !cartItems[id] ? (
+                        <img
+                            className='add'
+                            onClick={() => addToCart(id)}
+                            src={assets.add_icon_white}
+                            alt=''
+                        />
+                    ) : (
+                        <div className='food-item-count'>
+                            <img onClick={() => removeFromCart(id)} src={assets.remove_icon_red} alt="" />
+                            <p>{cartItems[id]}</p>
+                            <img onClick={() => addToCart(id)} src={assets.add_icon_green} alt="" />
+                        </div>
+                    )
+                }
             </div>
-            <p className='food-item-description'>
-                {description}
-            </p>
-            <p className="food-item-price">
-                <span>₹</span>
-                {price}
-            </p>
-            </div>
-    </div>
-  )
-}
 
-export default FoodItem
+            <div className="food-item-info">
+                <div className="food-item-name-rating">
+                    <p>{name}</p>
+                    <img src={assets.rating_starts} alt="" />
+                </div>
+                <p className='food-item-description'>{description}</p>
+                <p className="food-item-price">
+                    <span>₹</span>{price}
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default FoodItem;
--- fooddelivery/src/Context/StoreContext.jsx
+++ fooddelivery/src/Context/StoreContext.jsx
@@ -1,21 +1,51 @@
-import { createContext } from "react";
+import React, { createContext, useEffect, useState } from 'react';
 import { food_list } from "../assets/frontend_assets/assets";
 
+// Create Context
+export const StoreContext = createContext(null);
+
+const StoreContextProvider = ({ children }) => {
+    const [cartItems, setCartItems] = useState({});
+
+    // Add to cart
+    const addToCart = (itemid) => {
+        console.log("Item added to cart:", itemid);
+        setCartItems(prev => ({
+            ...prev,
+            [itemid]: (prev[itemid] || 0) + 1
+        }));
+    };
+
+    // Remove from cart
+    const removeFromCart = (itemid) => {
+        console.log("Item removed from cart:", itemid);
+        setCartItems(prev => ({
+            ...prev,
+            [itemid]: prev[itemid] > 1 ? prev[itemid] - 1 : 0
+        }));
+    };
 
 
-export const StoreContext = createContext(null)
+    useEffect(() => {
+        // Initialize cart items from localStorage if available
+        // const storedCart = JSON.parse(localStorage.getItem('cartItems')) || {};
+        // setCartItems(storedCart);
+        console.log("Cart items initialized:", cartItems);
+    }, [cartItems]);
 
-
-const StoreContextProvider = ({ children }) => {   
+    // Provide value to children
     const contextValue = {
-       food_list
-    };  
+        food_list,
+        cartItems,
+        addToCart,
+        removeFromCart
+    };
 
     return (
         <StoreContext.Provider value={contextValue}>
             {children}
         </StoreContext.Provider>
     );
-}
+};
 
 export default StoreContextProvider;
